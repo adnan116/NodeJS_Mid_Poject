71744547e29c3f36a65b80b2549fe54baf30fedd
Khandakar Anim Hassan Adnan <khandakar.adnan21@gmail.com>
2020-03-04 23:42:48 +0600

    student unblocking with confirmation added

--- models/student-model.js
+++ models/student-model.js
@@ -45,6 +45,17 @@ module.exports ={
 		});
 	},
 
+	getAllInactiveStudents:function(callback){
+		var sql = "select * from students where status = 'inactive'";
+		db.getResult(sql, null, function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback(null);
+			}
+		});
+	},
+
 	getById: function(id, callback){
 		var sql = "select * from students where id=?";
 		db.getResult(sql, [id], function(result){
@@ -80,6 +91,17 @@ module.exports ={
 
 	blockStudent: function(id, callback){
 		var sql = "update students set status = 'inactive' where id=?";
+		db.execute(sql, [id], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+
+	unblockStudent: function(id, callback){
+		var sql = "update students set status = 'active' where id=?";
 		db.execute(sql, [id], function(status){
 			if(status){
 				callback(true);
